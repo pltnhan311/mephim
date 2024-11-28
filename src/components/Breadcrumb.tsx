62082297c99518afc40c6664666738456b264d97
pltnhan311 <92429264+pltnhan311@users.noreply.github.com>
2024-11-28 15:35:28 +0700

    fix disable click breadcrumb

--- src/components/Breadcrumb.tsx
+++ src/components/Breadcrumb.tsx
@@ -1,27 +1,27 @@
 import { faChevronRight, faHome } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import { BreadCrumb } from '~/types/movie/movie-types'
 
 const Breadcrumb = ({ breadCrumb }: { breadCrumb: BreadCrumb[] }) => {
   return (
     <div className='flex items-center space-x-2 text-xs sm:text-sm px-1'>
-      <Link to='/' className='text-gray-400 hover:text-white transition-colors duration-300'>
+      <NavLink to='/' className='text-gray-400 hover:text-white transition-colors duration-300'>
         <FontAwesomeIcon icon={faHome} className='mr-1' />
         Trang chủ
-      </Link>
+      </NavLink>
       {/* Limit to 3 items after Home, for a total of 4 */}
       {breadCrumb?.slice(0, 3).map((item, index) => (
         <div key={`${item._id}-${index}`} className='flex items-center'>
           <FontAwesomeIcon icon={faChevronRight} className='text-gray-500 mx-2' />
-          <Link
-            to={`/${item.slug}`}
+          <p
+            // to={`/${item.slug}`}
             className={`${
               index === Math.min(breadCrumb.length - 1, 2) ? 'text-orange-400' : 'text-gray-400 hover:text-white'
             } transition-colors duration-300`}
           >
             {item.name}
-          </Link>
+          </p>
         </div>
       ))}
       {/* Show ellipsis if there are more than 3 items */}
